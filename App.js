8ee2c445c911b2b51bd739b8a9d89ba69d80a16a
Tal Mekler <tal.mekler11@gmail.com>
2022-12-12 22:22:39 +0200

    add options to app

--- App.js
+++ App.js
@@ -1,49 +1,79 @@
+function printGame(player, board) {
+  // TODO: print player stat
+  board.print();
+}
+
 // const board = new Board();
 const player = new Player();
+// const board = new Board();
+let gameOn = true;
 console.log(player.getLocation());
 
 // document.addEventListener("keypress", () => {
 
 // })
 
-document.onkeydown = checkKey;
+if (gameOn) {
+  document.onkeydown = checkKey;
 
-function checkKey(e) {
+  function checkKey(e) {
     let isMoveSuccess = false;
-  e = e || window.event;
+    e = e || window.event;
 
-  if (e.keyCode == "38" || e.key == "w" || e.key == "W") {
-    // up arrow
-    isMoveSuccess = player.moveUp();
-  } else if (e.keyCode == "40" || e.key == "s" || e.key == "S") {
-    // down arrow
-    isMoveSuccess = player.moveDown();
-  } else if (e.keyCode == "37" || e.key == "a" || e.key == "A") {
-    // left arrow
-    isMoveSuccess = player.moveLeft();
-  } else if (e.keyCode == "39" || e.key == "d" || e.key == "D") {
-    // right arrow
-    isMoveSuccess = player.moveRight();
-  }
-
-  if(isMoveSuccess){
-    // Move success
-    // TODO: Board::isMonsterOnSquare() -> start fight -> if 0 HP to player = GAME OVER || else -> print stats
-    const monsterInPosition = getMonsterInPosition(player.getLocation.x, player.getLocation.y)
-    if(monsterInPosition != null){
-      player.startFight(monsterInPosition)
-      if(player.getHp === 0){
-        //Player dead
-        console.log("GAME OVER - YOU LOSE!");
-      }else {
-        // Player killed the monster
-        player.setMonsterKilled();
-      }
+    if (e.keyCode == "38" || e.key == "w" || e.key == "W") {
+      // up arrow
+      isMoveSuccess = player.moveUp();
+    } else if (e.keyCode == "40" || e.key == "s" || e.key == "S") {
+      // down arrow
+      isMoveSuccess = player.moveDown();
+    } else if (e.keyCode == "37" || e.key == "a" || e.key == "A") {
+      // left arrow
+      isMoveSuccess = player.moveLeft();
+    } else if (e.keyCode == "39" || e.key == "d" || e.key == "D") {
+      // right arrow
+      isMoveSuccess = player.moveRight();
     }
 
-    // TODO: Board::isItemOnSquare() -> take item -> print stats
+    if (isMoveSuccess) {
+      // Move success
+      
 
-    // TODO: Board::isOnEndSquare() -> WIN! -> ask to play again
-    
+      // TODO: Board::isMonsterOnSquare() -> start fight -> if 0 HP to player = GAME OVER || else -> print stats
+      const monsterInPosition = board.getMonsterInPosition(
+        player.getLocation.x,
+        player.getLocation.y
+      );
+      const itemInPosition = board.getMonsterInPosition(
+        player.getLocation.x,
+        player.getLocation.y
+      );
+      
+      if (monsterInPosition != null) {
+        player.startFight(monsterInPosition);
+        if (player.getHp === 0) {
+          //Player dead
+          gameOn = false;
+          console.log("GAME OVER - YOU LOSE!");
+        } else {
+          // Player killed the monster
+          player.setMonsterKilled();
+        }
+      }
+
+      // TODO: Board::isItemOnSquare() -> take item -> print stats
+      else if (itemInPosition != null) {
+        player.takeItem(itemInPosition);
+      }
+
+      // TODO: isOnEndSquare() -> WIN! -> ask to play again
+      else if (player.getLocation.x === 24 && player.getLocation.y === 24) {
+        gameOn = false;
+        console.log("YOU WIN!");
+      }
+
+      printGame(player, board);
+    }
   }
+}else {
+  console.log("Press on keyboard to restart");
 }
